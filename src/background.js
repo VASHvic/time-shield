47979de6729373317cc2165f2af64ad773707d0f
VASH <vforvanish@gmail.com>
2022-12-20 19:56:34 +0100

    feat: extension works
    check why service worker goes idle

--- src/background.js
+++ src/background.js
@@ -1,47 +1,99 @@
-var twitterActive = false;
-var twitterTimer =0;
-const interValEnd = setInterval(()=> {
-    Boolean(twitterActive) && (twitterTimer+=10);
-    console.log({twitterActive,twitterTimer});
-},10000);
+async function main() {
+    const dayToday = new Date().getDay();
+    const { twitter, today } = await chrome.storage.local.get(["twitter", "today"]);
+    let twitterTimer = typeof twitter === "number" ? twitter : 0;
+    console.log({ twitter, today, twitterTimer });
+    if (today !== dayToday) {
+        console.log("els dies son diferents y la variable de temps es resetea");
+        twitterTimer = 0;
+    } else {
+        console.log("estem en el mateix dia y la variable es queda igual");
+    }
+    let twitterActive = false;
+    let interValEnd;
 
-function readTabName(t){
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        console.log(tabs[0]?.url);
-        if(tabs[0]?.url.includes("twitter")){
-            twitterActive=true;
-        }else{
-            twitterActive=false;
-            // guardar en storage el temps
+
+    function readTabName(t) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            console.log(tabs[0]?.url);
+            if (tabs[0]?.url.includes("twitter")) {
+                console.log("la boleana de interbal id", !Boolean(interValEnd));
+                if (!Boolean(interValEnd)) {
+                    chrome.storage.local.get(["twitter","today"]).then(console.log)
+                    twitterActive = true;
+                    console.log("interval estaba buit i ne tinc que crear uno nou");
+                    creteInterVal()
+                }
+            } else {
+                console.log("are tindria que borrar o no tocar el intervalo");
+                twitterActive = false;
+                chrome.storage.local.set({
+                    "twitter": twitterTimer,
+                    "today": dayToday
+
+                });
+                chrome.storage.local.get(["twitter","today"]).then(console.log)
+                Boolean(interValEnd) && clearRequestedInterval(interValEnd);
+            }
         }
+        )
+    }
 
-       }
-       )
+    function creteInterVal() {
+        console.log("intervalEnd a boleana per a saber si ja existeix", Boolean(interValEnd));
+        if (Boolean(interValEnd)) return;
+        console.log("create Interval s ha cridat");
+        interValEnd = setInterval(() => {
+            twitterActive && (twitterTimer += 1);
+            chrome.action.setBadgeText({
+                text: `${twitterTimer}` // dividir entre 60 si vull traure els minuts
+            });
+            if (twitterActive && twitterTimer >= 1800) {
+                createAlarm()
+                chrome.storage.local.get(["twitter","today"]).then(console.log)
+
+
+            }
+            console.log({ dayToday, twitterActive, twitterTimer });
+        }, 1000);
+    }
+
+
+
+
+
+    function createAlarm() {
+        chrome.alarms.create({
+            when: new Date().getMilliseconds()
+        });
+    }
+    function clearRequestedInterval(clearRequestedInterval) {
+        console.log(`borrant interval ${clearRequestedInterval}`);
+        clearInterval(clearRequestedInterval);
+        interValEnd = undefined;
+        console.log("intervalEnd despues de borrarla val ", interValEnd);
+    }
+
+    chrome.windows.onFocusChanged.addListener(readTabName);
+
+    chrome.tabs.onActivated.addListener(readTabName);
+
+    chrome.runtime.onSuspend.addListener(() => {
+        chrome.storage.local.set({
+            "twitter": twitterTimer,
+            "today": dayToday
+        });
+        clearRequestedInterval(interValEnd)
+
+    });
+
+    chrome.alarms.onAlarm.addListener((alarm) => {
+        this.registration.showNotification("Time Guardian Extension", {
+            body: "Close Twitter",
+            icon: "shield.png"
+        });
+    });
 }
 
-chrome.windows.onFocusChanged.addListener(readTabName);
 
-chrome.tabs.onActivated.addListener(readTabName)
-// chrome.runtime.onSuspend.addListener() guardar timer y tancar timeout
-
-chrome.alarms.create({
-  periodInMinutes: 1/60,
-});
-
-chrome.alarms.onAlarm.addListener((alarm)=> {
-    
-//   chrome.storage.local.get(["timer"], (res)=> {
-    // const time = res.timer ?? 0;
-    // chrome.storage.local.set({
-    //   timer: time +1
-    // });   
-    // chrome.action.setBadgeText({
-    //   text:`${time + 1}`
-    // });
-
-    this.registration.showNotification("Time Guardian Extension", {
-      body: "Close Twitter",
-      icon: "shield.png"
-    })
-//   });
-});
+main()
