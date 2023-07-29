8de6cad419e7cd9e7460181b4e6efcb3297e7535
VASH <vforvanish@gmail.com>
2023-07-29 19:03:33 +0200

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-use-before-define': ['error', {
+      functions: false,
+    }],
+  },
+
+};
